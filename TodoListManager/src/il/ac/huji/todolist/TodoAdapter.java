6fb6232074d5ba593d196074955fa9c3fef1eba4
Beni Lev <beniloew@gmail.com>
2013-03-15 13:13:47 +0200

    Final version. some small tunings.
    Signed-off-by: Beni Lev <beniloew@gmail.com>

--- TodoListManager/src/il/ac/huji/todolist/TodoAdapter.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoAdapter.java
@@ -14,22 +14,19 @@ import android.widget.TextView;
 public class TodoAdapter extends ArrayAdapter<String> {
 	
 	private List<String> items;
-	private Context context;
 	
 	public TodoAdapter(Context context, int textViewResourceId) {
 		super(context, textViewResourceId);
 		items = new ArrayList<String>();
-		this.context = context;
 	}
 
 	public TodoAdapter(Context context, int textViewResourceId, List<String> objects) {
 		super(context, textViewResourceId, objects);
 		items = objects;
-		this.context = context;
 	}
 
 	public View getView(int position, View convertView, ViewGroup parent) {
-	      LayoutInflater inflater = LayoutInflater.from(context);
+	      LayoutInflater inflater = LayoutInflater.from(getContext());//context);
 	      TextView txt = (TextView) inflater.inflate(R.layout.list_item, parent, false);
 	      txt.setText(items.get(position));
 	      txt.setTextColor(position % 2 == 0 ? Color.RED : Color.BLUE);
@@ -38,5 +35,11 @@ public class TodoAdapter extends ArrayAdapter<String> {
 	
 	public void add(String str) {
 		items.add(str);
+		notifyDataSetChanged();
+	}
+	
+	public void remove(String str) {
+		items.remove(str);
+		notifyDataSetChanged();
 	}
 }
--- TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -4,31 +4,20 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class TodoListManagerActivity extends Activity {
 
 	private TodoAdapter adapter;
-	private ArrayAdapter<String> adapter2;
-	ArrayList<String> strs;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_todo_list_manager);
-		strs = new ArrayList<String>();
-		strs.add("Beni");
-		String[] bla = {"dsadsa", "dsadsa"};
-		adapter = new TodoAdapter(this, R.layout.list_item, strs);
-		adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, bla);
+		adapter = new TodoAdapter(this, R.layout.list_item, new ArrayList<String>());
 		ListView list = (ListView)findViewById(R.id.lstTodoItems);
 		list.setAdapter(adapter);
 	}
@@ -55,17 +44,13 @@ public class TodoListManagerActivity extends Activity {
 
 	private void deleteSelectedItem() {
 		ListView list = (ListView)findViewById(R.id.lstTodoItems);
-		TextView item = (TextView) list.getSelectedView();
-		if (item == null) return;
-		String itemText = item.getText().toString();
-		strs.remove(itemText);
-		adapter.notifyDataSetChanged();
+		String itemText = (String)list.getSelectedItem();
+		adapter.remove(itemText);
 	}
 
 	private void addToList() {
 		EditText edtText = (EditText)findViewById(R.id.edtNewItem);
 		String text = edtText.getText().toString();
-		strs.add(text);
-		adapter.notifyDataSetChanged();
+		adapter.add(text);
 	}
 }
